b95a951beb728dd4a3a849d97a1357686a970cdf
juyeongjung <juyeong.dev@gmail.com>
2021-03-01 16:45:41 +0900

    2178bfs(fail)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Objects;
-import java.util.Queue;
 
 public class Main {
 
@@ -24,75 +21,30 @@ public class Main {
             }
         }
 
-        Queue<Point> queue = new LinkedList<>();
-        int count = bfs(network, visited, queue, x, y);
-        System.out.println(count);
+        visited[1][1] = 1;
+        int dfs = dfs(network, visited, 1, 1, x, y);
+
+        System.out.println(dfs);
     }
 
-
-    private static int bfs(int[][] network, int[][] visited, Queue<Point> queue, int endX, int endY) {
+    private static int dfs(int[][] network, int[][] visited, int nowX, int nowY, int endX, int endY) {
         int[] directionX = {1, 0, -1, 0};
         int[] directionY = {0, 1, 0, -1};
-        visited[1][1] = 1;
-        queue.add(new Point(1, 1));
-        while (!queue.isEmpty()) {
-            Point now = queue.remove();
-            if (new Point(endX, endY).equals(now)) {
+        for (int i = 0; i < directionX.length; i++) {
+            int nextX = nowX + directionX[i];
+            int nextY = nowY + directionY[i];
+            if (nextX == endX && nextY == endY) {
+                visited[nextX][nextY] = visited[nowX][nowY] + 1;
                 return visited[endX][endY];
             }
-            for (int i = 0; i < directionX.length; i++) {
-                int nextX = now.getX() + directionX[i];
-                int nextY = now.getY() + directionY[i];
-
-                if (nextX < 1 || nextY < 1 || nextX > endX || nextY > endY) {
-                    continue;
-                }
-                if (visited[nextX][nextY] != 0) {
-                    continue;
-                }
-                if (network[nextX][nextY] == 0) {
-                    continue;
-                }
-                visited[nextX][nextY] = visited[now.getX()][now.getY()] + 1;
-                queue.add(new Point(nextX, nextY));
+            if (nextX < 1 || nextX > endX || nextY < 1 || nextY > endY) {
+                continue;
+            }
+            if (network[nextX][nextY] == 1 && visited[nextX][nextY] == 0) {
+                visited[nextX][nextY] = visited[nowX][nowY] + 1;
+                dfs(network, visited, nextX, nextY, endX, endY);
             }
         }
-        return 0;
-    }
-}
-
-class Point {
-
-    private final int x;
-    private final int y;
-
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Point point = (Point) o;
-        return x == point.x && y == point.y;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y);
+        return visited[endX][endY];
     }
 }
