52f5cf3d8305792882845cf89c5bf107a70815e0
juyeongjung <juyeong.dev@gmail.com>
2021-03-12 17:39:13 +0900

    Correct

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,39 +8,17 @@ public class Main {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String[] line1 = reader.readLine().split("");
         String[] line2 = reader.readLine().split("");
-        int[][] cache = new int[line1.length][line2.length];
+        int[][] cache = new int[line1.length + 1][line2.length + 1];
 
-        if (line1[0].equals(line2[0])) {
-            cache[0][0] = 0;
-        } else {
-            cache[0][0] = 1;
-        }
-
-        for (int i = 1; i < line2.length; i++) {
-            if (line1[0].equals(line2[i])) {
-                cache[0][i] = cache[0][i - 1];
-            } else {
-                cache[0][i] = cache[0][i - 1] + 1;
-            }
-        }
-
-        for (int i = 1; i < line1.length; i++) {
-            if (line1[i].equals(line2[0])) {
-                cache[i][0] = cache[i - 1][0];
-            } else {
-                cache[i][0] = cache[i - 1][0] + 1;
-            }
-        }
-
-        for (int i = 1; i < line1.length; i++) {
-            for (int j = 1; j < line2.length; j++) {
-                if (!line1[i].equals(line2[j])) {
-                    cache[i][j] = Math.min(cache[i - 1][j - 1], Math.min(cache[i][j - 1], cache[i - 1][j])) + 1;
+        for (int i = 1; i <= line1.length; i++) {
+            for (int j = 1; j <= line2.length; j++) {
+                if (line1[i - 1].equals(line2[j - 1])) {
+                    cache[i][j] = cache[i - 1][j - 1] + 1;
                 } else {
-                    cache[i][j] = cache[i - 1][j - 1];
+                    cache[i][j] = Math.max(cache[i][j - 1], cache[i - 1][j]);
                 }
             }
         }
-        System.out.println(cache[line1.length - 1][line2.length - 1]);
+        System.out.println(cache[line1.length][line2.length]);
     }
 }
