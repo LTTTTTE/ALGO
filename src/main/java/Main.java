9111509cf261bffbf87a78e1b8e8355d452e5166
juyeongjung <juyeong.dev@gmail.com>
2021-03-12 17:04:17 +0900

    fail

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int count = Integer.parseInt(reader.readLine());
-        int[] numbers = Arrays.stream(reader.readLine().split(" "))
-            .mapToInt(Integer::parseInt)
-            .toArray();
-        List<Integer> answers = new ArrayList<>();
+        String[] line1 = reader.readLine().split("");
+        String[] line2 = reader.readLine().split("");
+        int[][] cache = new int[line1.length][line2.length];
 
-        for (int i = 0; i < numbers.length; i++) {
-            if (i == 0) {
-                answers.add(numbers[i]);
-                continue;
-            }
-            if (answers.get(i - 1) > 0) {
-                answers.add(answers.get(i - 1) + numbers[i]);
+        if (line1[0].equals(line2[0])) {
+            cache[0][0] = 0;
+        } else {
+            cache[0][0] = 1;
+        }
+
+        for (int i = 1; i < line2.length; i++) {
+            if (line1[0].equals(line2[i])) {
+                cache[0][i] = cache[0][i - 1];
             } else {
-                answers.add(numbers[i]);
+                cache[0][i] = cache[0][i - 1] + 1;
             }
         }
-        System.out.println(answers.stream().mapToInt(x -> x).max().getAsInt());
+
+        for (int i = 1; i < line1.length; i++) {
+            if (line1[i].equals(line2[0])) {
+                cache[i][0] = cache[i - 1][0];
+            } else {
+                cache[i][0] = cache[i - 1][0] + 1;
+            }
+        }
+
+        for (int i = 1; i < line1.length; i++) {
+            for (int j = 1; j < line2.length; j++) {
+                if (!line1[i].equals(line2[j])) {
+                    cache[i][j] = Math.min(cache[i - 1][j - 1], Math.min(cache[i][j - 1], cache[i - 1][j])) + 1;
+                } else {
+                    cache[i][j] = cache[i - 1][j - 1];
+                }
+            }
+        }
+        System.out.println(cache[line1.length - 1][line2.length - 1]);
     }
 }
-
