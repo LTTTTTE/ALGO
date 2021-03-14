6bcea1e64d4a41f132251cff73e4239de6a1703f
juyeongjung <juyeong.dev@gmail.com>
2021-03-14 16:52:41 +0900

    temp

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,38 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String[] line1 = reader.readLine().split("");
-        String[] line2 = reader.readLine().split("");
-        int[][] cache = new int[line1.length + 1][line2.length + 1];
-
-        if (line1[0].equals(line2[0])) {
-            cache[1][1] = 0;
-        } else {
-            cache[1][1] = 1;
+        String[] sizes = reader.readLine().split(" ");
+        int boundX = Integer.parseInt(sizes[0]);
+        int boundY = Integer.parseInt(sizes[1]);
+        int[][] network = new int[boundX][boundY];
+        for (int i = 0; i < boundX; i++) {
+            network[i] = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         }
 
-        for (int i = 0; i < line2.length; i++) {
-            cache[0][i + 1] = cache[0][i] + 1;
-        }
-
-        for (int i = 0; i < line1.length; i++) {
-            cache[i + 1][0] = cache[i][0] + 1;
-        }
-
-        for (int i = 1; i <= line1.length; i++) {
-            for (int j = 1; j <= line2.length; j++) {
-                if (!line1[i - 1].equals(line2[j - 1])) {
-                    cache[i][j] = Math.min(cache[i - 1][j - 1], Math.min(cache[i][j - 1], cache[i - 1][j])) + 1;
-                } else {
-                    cache[i][j] = cache[i - 1][j - 1];
+        for (int i = 0; i < boundX; i++) {
+            for (int j = 0; j < boundY; j++) {
+                if (network[i][j] == 0) {
+                    network[i][j] = 1;
                 }
             }
         }
-        System.out.println(cache[line1.length][line2.length]);
     }
 }
