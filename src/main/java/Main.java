f57a7f012eb5da210c50804a6cedf427458ed853
juyeongjung <juyeong.dev@gmail.com>
2021-03-15 12:45:50 +0900

    Correct

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String[] inputs = reader.readLine().split(" ");
-        int species = Integer.parseInt(inputs[0]);
-        int charge = Integer.parseInt(inputs[1]);
-        int[] coins = new int[species];
-        for (int i = 0; i < species; i++) {
-            coins[i] = Integer.parseInt(reader.readLine());
+        int size = Integer.parseInt(reader.readLine());
+        Queue<int[]> meets = new PriorityQueue<>(Comparator.comparingInt(x -> ((int[]) x)[1])
+            .thenComparingInt(x -> ((int[]) x)[0]));
+        for (int i = 0; i < size; i++) {
+            String[] inputs = reader.readLine().split(" ");
+            meets.add(new int[]{Integer.parseInt(inputs[0]), Integer.parseInt(inputs[1])});
         }
         int count = 0;
-        while (charge != 0) {
-            for (int i = species - 1; i >= 0; i--) {
-                while(coins[i] <= charge) {
-                    charge -= coins[i];
-                    count++;
-                }
+        int endTime = 0;
+        while (!meets.isEmpty()) {
+            int[] meet = meets.remove();
+            if (meet[0] >= endTime) {
+                endTime = meet[1];
+                count++;
+                continue;
+            }
+            if (meet[1] < endTime) {
+                meets.add(meet);
+                endTime = meet[1];
             }
         }
         System.out.println(count);
