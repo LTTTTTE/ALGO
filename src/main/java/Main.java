b4b5e467f09b3d70138cb4b4c6d5c86375842783
juyeongjung <juyeong.dev@gmail.com>
2021-03-15 21:27:09 +0900

    fail

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,81 +2,102 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
-    static int maxCount = 0;
+
+    static int boundX = 0;
+    static int boundY = 0;
+    static int canCrash = 0;
+    static int[][] network;
+    static boolean[][] visited;
+    static int min = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String[] sizes = reader.readLine().split(" ");
-        int boundX = Integer.parseInt(sizes[0]);
-        int boundY = Integer.parseInt(sizes[1]);
-        int[][] network = new int[boundX][boundY];
-        int[][] safe = new int[boundX][boundY];
+        String[] inputs = reader.readLine().split(" ");
+        boundX = Integer.parseInt(inputs[0]);
+        boundY = Integer.parseInt(inputs[1]);
+        canCrash = Integer.parseInt(inputs[2]);
+        network = new int[boundX][boundY];
+        visited = new boolean[boundX][boundY];
         for (int i = 0; i < boundX; i++) {
-            network[i] = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            network[i] = Arrays.stream(reader.readLine().split("")).mapToInt(Integer::parseInt).toArray();
         }
 
         for (int i = 0; i < boundX; i++) {
             for (int j = 0; j < boundY; j++) {
-                if (network[i][j] == 0) {
+                if (network[i][j] == 1) {
+                    network[i][j] = 2;
+                    canCrash--;
+                    crashWall(network, i, j);
                     network[i][j] = 1;
-                    placeWall(network, safe, new Point(i, j), 2);
-                    network[i][j] = 0;
+                    canCrash++;
                 }
             }
         }
-        System.out.println(maxCount);
+        if (min == Integer.MAX_VALUE) {
+            System.out.println(-1);
+        } else {
+            System.out.println(min);
+        }
     }
 
-    private static void placeWall(int[][] network, int[][] safe, Point beforePoint, int leftCount) {
-        if (leftCount == 0) {
-            safe = new int[network.length][network[0].length];
-            for (int i = 0; i < network.length; i++) {
-                for (int j = 0; j < network[0].length; j++) {
-                    if (network[i][j] == 2) {
-                        safe[i][j] = 2;
-                        spreadVirus(network, safe, i, j);
-                    }
-                }
+    private static void crashWall(int[][] network, int x, int y) {
+        if (canCrash == 0) {
+            visited = new boolean[boundX][boundY];
+            visited[0][0] = true;
+            int bfs = bfs(network, 0, 0);
+            if (bfs != -1) {
+                min = Integer.min(bfs, min);
             }
-            int count = 0;
-            for (int i = 0; i < safe.length; i++) {
-                for (int j = 0; j < safe[0].length; j++) {
-                    if (safe[i][j] == 0 && network[i][j] == 0) {
-                        count++;
-                    }
-                }
-            }
-            maxCount = Math.max(count, maxCount);
             return;
         }
-        int beforeX = beforePoint.getX();
-        int beforeY = beforePoint.getY() + 1;
-        for (int i = beforeX; i < network.length; i++) {
-            for (int j = beforeY; j < network[0].length; j++) {
-                if (network[i][j] == 0) {
+        int nextY = y + 1;
+        for (int i = x; i < boundX; i++) {
+            for (int j = nextY; j < boundY; j++) {
+                if (network[i][j] == 1) {
+                    network[i][j] = 2;
+                    canCrash--;
+                    crashWall(network, x, y);
                     network[i][j] = 1;
-                    placeWall(network, safe, new Point(i, j), leftCount - 1);
-                    network[i][j] = 0;
+                    canCrash++;
                 }
             }
-            beforeY = 0;
+            nextY = 0;
         }
     }
 
-    private static void spreadVirus(int[][] network, int[][] safe, int x, int y) {
-        int[] directionX = {1, -1, 0, 0};
-        int[] directionY = {0, 0, 1, -1};
-        for (int i = 0; i < directionX.length; i++) {
-            int nextX = x + directionX[i];
-            int nextY = y + directionY[i];
-            if (nextX < network.length && nextY < network[0].length && nextX >= 0 && nextY >= 0
-                && network[nextX][nextY] == 0 && safe[nextX][nextY] == 0) {
-                safe[nextX][nextY] = 2;
-                spreadVirus(network, safe, nextX, nextY);
+    private static int bfs(int[][] network, int x, int y) {
+        Queue<Point> queue = new LinkedList<>();
+        int count = 1;
+        queue.add(new Point(x, y));
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+        while (!queue.isEmpty()) {
+            int queueSize = queue.size();
+            for (int k = 0; k < queueSize; k++) {
+                Point now = queue.remove();
+                for (int i = 0; i < dx.length; i++) {
+                    if (now.getX() == boundX - 1 && now.getY() == boundY - 1) {
+                        return count;
+                    }
+                    int nextX = now.getX() + dx[i];
+                    int nextY = now.getY() + dy[i];
+                    if (nextX < boundX && nextY < boundY && nextX >= 0 && nextY >= 0
+                        && network[nextX][nextY] != 1 && !visited[nextX][nextY]) {
+                        if (network[nextX][nextY] == 2 && count % 2 == 0) {
+                            count++;
+                        }
+                        visited[nextX][nextY] = true;
+                        queue.add(new Point(nextX, nextY));
+                    }
+                }
             }
+            ++count;
         }
+        return -1;
     }
 }
 
