365371ff1426820116561eadfe181c1f166f4b84
juyeongjung <juyeong.dev@gmail.com>
2021-03-15 18:01:20 +0900

    Correct

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,70 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int classes = Integer.parseInt(reader.readLine());
+        int[] classMates = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         String[] inputs = reader.readLine().split(" ");
-        int shopCount = Integer.parseInt(inputs[0]);
-        int bagCount = Integer.parseInt(inputs[1]);
-
-        List<Jewelry> jewelries = new ArrayList<>();
-        Queue<Integer> bags = new PriorityQueue<>(Integer::compare);
-
-        for (int i = 0; i < shopCount; i++) {
-            jewelries.add(new Jewelry(reader.readLine()));
-        }
-        for (int i = 0; i < bagCount; i++) {
-            bags.add(Integer.parseInt(reader.readLine()));
-        }
-
-        jewelries.sort(Comparator.comparingInt(Jewelry::getWeight));
-
-        Queue<Jewelry> answer = new PriorityQueue<>(Comparator.comparingInt(Jewelry::getPrice).reversed());
-        List<Jewelry> total = new ArrayList<>();
-        int j = 0;
-        for (int i = 0; i < bagCount; i++) {
-            int bag = bags.remove();
-            for (; j < shopCount; j++) {
-                Jewelry jewelry = jewelries.get(j);
-                if (jewelry.getWeight() <= bag) {
-                    answer.add(jewelries.get(j));
-                } else {
-                    break;
-                }
-            }
-
-            if (!answer.isEmpty()) {
-                total.add(answer.remove());
-            }
-        }
-        System.out.println(total.stream().mapToLong(Jewelry::getPrice).sum());
-    }
-}
-
-class Jewelry {
-
-    private int weight;
-    private int price;
-
-    public Jewelry(String input) {
-        String[] s = input.split(" ");
-        this.weight = Integer.parseInt(s[0]);
-        this.price = Integer.parseInt(s[1]);
-    }
-
-    public int getWeight() {
-        return weight;
-    }
-
-    public int getPrice() {
-        return price;
+        int masterCan = Integer.parseInt(inputs[0]);
+        int secondCan = Integer.parseInt(inputs[1]);
+        long sum = Arrays.stream(classMates)
+            .map(x -> x - masterCan)
+            .map(x -> Math.max(x, 0))
+            .map(x -> (int) Math.ceil((double) x / secondCan))
+            .mapToLong(x -> x)
+            .sum();
+        System.out.println(sum + (long) classes);
     }
 }
