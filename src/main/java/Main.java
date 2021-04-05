73d5c53b4fe1e13505cb5eeb02ed1afb745bdce2
juyeongjung <juyeong.dev@gmail.com>
2021-04-05 16:49:59 +0900

    Correct - findPoint 시 point 잘음직이자

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,67 +1,104 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
 
-    public static void main(String[] args) {
-        solution(6);
-    }
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static char[][] network;
+    static boolean[][] visited;
+    static int size;
 
-    public static int[] solution(int size) {
-        int[][] snail = new int[size][];
-        int number = 0;
-        int topBound = 0;
-        int leftBound = 0;
-        int bottomBound = size - 1;
-        int rightBound = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        size = Integer.parseInt(reader.readLine());
+
+        network = new char[size][size];
+        visited = new boolean[size][size];
+        int count = 0;
+        int badCount = 0;
+
         for (int i = 0; i < size; i++) {
-            snail[i] = new int[i + 1];
+            network[i] = reader.readLine().toCharArray();
         }
 
-        while (containZero(snail)) {
-
-
-
-            for (int i = topBound; i <= bottomBound; i++) {
-                snail[i][leftBound] = ++number;
-            }
-            leftBound++;
-            topBound++;
-
-
-
-            for (int i = leftBound; i < snail[bottomBound].length - rightBound; i++) {
-                snail[bottomBound][i] = ++number;
-            }
-            bottomBound--;
-
-
-
-            for (int i = bottomBound; i >= topBound; i--) {
-                snail[i][snail[i].length - rightBound - 1] = ++number;
-            }
-            rightBound++;
-            topBound++;
-
-
+        Point point = new Point(0, 0);
+        while (point != null) {
+            count += bfs(visited, point);
+            point = findPoint(point);
         }
-        List<Integer> answer = new ArrayList<>();
-        for (int i = 0; i < snail.length; i++) {
-            for (int j = 0; j < snail[i].length; j++) {
-                answer.add(snail[i][j]);
-            }
-        }
-        return answer.stream().mapToInt(x -> x).toArray();
-    }
 
-    public static boolean containZero(int[][] snail) {
-        for (int i = 0; i < snail.length; i++) {
-            for (int j = 0; j < snail[i].length; j++) {
-                if (snail[i][j] == 0) {
-                    return true;
+        point = new Point(0, 0);
+        visited = new boolean[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (network[i][j] == 'G') {
+                    network[i][j] = 'R';
                 }
             }
         }
-        return false;
+        while (point != null) {
+            badCount += bfs(visited, point);
+            point = findPoint(point);
+        }
+
+        System.out.println(count + " " + badCount);
+    }
+
+    public static int bfs(boolean[][] visited, Point startPoint) {
+        Queue<Point> queue = new LinkedList<>();
+        queue.add(startPoint);
+
+        while (!queue.isEmpty()) {
+            Point now = queue.remove();
+            for (int i = 0; i < dx.length; i++) {
+                int nextX = now.getX() + dx[i];
+                int nextY = now.getY() + dy[i];
+                if (nextX < 0 || nextY < 0 || nextX >= size || nextY >= size) {
+                    continue;
+                }
+                if (!visited[nextX][nextY] && network[nextX][nextY] == network[now.getX()][now.getY()]) {
+                    visited[nextX][nextY] = true;
+                    queue.add(new Point(nextX, nextY));
+                }
+            }
+        }
+        return 1;
+    }
+
+    public static Point findPoint(Point beforePoint) {
+        int beforeX = beforePoint.getX();
+        int beforeY = beforePoint.getY() + 1;
+        for (int i = beforeX; i < size; i++) {
+            for (int j = beforeY; j < size; j++) {
+                if (!visited[i][j]) {
+                    return new Point(i, j);
+                }
+            }
+            beforeY = 0;
+        }
+        return null;
+    }
+}
+
+class Point {
+
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 }
