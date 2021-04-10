c7369a1fa4718e5171487a356d0d530912ac32a5
juyeongjung <juyeong.dev@gmail.com>
2021-04-10 16:09:57 +0900

    Correct - BufferedWriter써야 맞는 똥문제

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,49 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String[] split = reader.readLine().split("((?<=(\\+|-)|(?=(\\+|-))))");
-        int answer = Integer.parseInt(split[0]);
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        int size = Integer.parseInt(reader.readLine());
 
-        for (int i = 1; i < split.length; i++) {
-            if (split[i].equals("-")) {
-                for (int j = i + 1; j < split.length; j = j + 2) {
-                    answer -= Integer.parseInt(split[j]);
-                }
-                break;
-            }
-            if (split[i].equals("+")) {
-                answer += Integer.parseInt(split[i + 1]);
-                i++;
-            }
+        PriorityQueue<Integer> topQueue = new PriorityQueue<>(Integer::compare);
+        PriorityQueue<Integer> botQueue = new PriorityQueue<>(Collections.reverseOrder());
+
+        int mid = 0;
+        int[] number = new int[size];
+        for (int i = 0; i < size; i++) {
+            number[i] = Integer.parseInt(reader.readLine());
         }
-        System.out.println(answer);
+
+        for (int i = 0; i < size; i++) {
+            int input = number[i];
+            if (i == 0) {
+                mid = input;
+                System.out.println(mid);
+                continue;
+            }
+            if (input > mid) {
+                topQueue.add(input);
+            } else {
+                botQueue.add(input);
+            }
+            if (botQueue.size() > topQueue.size()) {
+                topQueue.add(mid);
+                mid = botQueue.remove();
+            } else if (botQueue.size() < topQueue.size() && i % 2 == 0) {
+                botQueue.add(mid);
+                mid = topQueue.remove();
+            }
+            writer.write(mid + "\n");
+        }
+        writer.flush();
+        writer.close();
     }
 }
