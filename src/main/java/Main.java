23a2f13cd0ca9f2c8c3d86732ab2997dd7a62b68
juyeongjung <juyeong.dev@gmail.com>
2021-04-10 17:36:36 +0900

    fail

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,49 +1,96 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Collections;
-import java.util.PriorityQueue;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-        int size = Integer.parseInt(reader.readLine());
+        String[] inputs = reader.readLine().split(" ");
+        int sizeX = Integer.parseInt(inputs[0]);
+        int sizeY = Integer.parseInt(inputs[1]);
 
-        PriorityQueue<Integer> topQueue = new PriorityQueue<>(Integer::compare);
-        PriorityQueue<Integer> botQueue = new PriorityQueue<>(Collections.reverseOrder());
+        int[][] network = new int[sizeX][sizeY];
+        int[][] visited = new int[sizeX][sizeY];
+        int[][] power = new int[sizeX][sizeY];
 
-        int mid = 0;
-        int[] number = new int[size];
-        for (int i = 0; i < size; i++) {
-            number[i] = Integer.parseInt(reader.readLine());
+        for (int i = 0; i < sizeX; i++) {
+            network[i] = Arrays.stream(reader.readLine().split("")).mapToInt(Integer::parseInt).toArray();
         }
 
-        for (int i = 0; i < size; i++) {
-            int input = number[i];
-            if (i == 0) {
-                mid = input;
-                System.out.println(mid);
-                continue;
-            }
-            if (input > mid) {
-                topQueue.add(input);
-            } else {
-                botQueue.add(input);
-            }
-            if (botQueue.size() > topQueue.size()) {
-                topQueue.add(mid);
-                mid = botQueue.remove();
-            } else if (botQueue.size() < topQueue.size() && i % 2 == 0) {
-                botQueue.add(mid);
-                mid = topQueue.remove();
-            }
-            writer.write(mid + "\n");
+        for (int i = 0; i < visited.length; i++) {
+            Arrays.fill(visited[i], Integer.MAX_VALUE);
         }
-        writer.flush();
-        writer.close();
+
+        Queue<Point> queue = new LinkedList<>();
+        queue.add(new Point(0, 0, 1, 1));
+        power[0][0] = 1;
+        visited[0][0] = 1;
+
+        int[] dx = {1, 0, -1, 0};
+        int[] dy = {0, 1, 0, -1};
+        while (!queue.isEmpty()) {
+            Point now = queue.remove();
+            for (int i = 0; i < dx.length; i++) {
+                int nextX = now.getX() + dx[i];
+                int nextY = now.getY() + dy[i];
+                if (nextX < 0 || nextY < 0 || nextX >= sizeX || nextY >= sizeY) {
+                    continue;
+                }
+                if (now.getDistance() > visited[nextX][nextY]) {
+                    continue;
+                }
+                if (now.getPower() == 1 && network[nextX][nextY] == 1) {
+//                    power[nextX][nextY] = 0;d
+                    queue.add(new Point(nextX, nextY,visited[now.getX()][now.getY()] + 1, 0));
+                    visited[nextX][nextY] = visited[now.getX()][now.getY()] + 1;
+                    continue;
+                }
+
+                if (network[nextX][nextY] == 0) {
+                    queue.add(new Point(nextX, nextY,visited[now.getX()][now.getY()] + 1, now.getPower()));
+                    visited[nextX][nextY] = visited[now.getX()][now.getY()] + 1;
+                }
+            }
+        }
+        if (visited[sizeX - 1][sizeY - 1] == 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(visited[sizeX - 1][sizeY - 1]);
+        }
+    }
+}
+
+class Point {
+
+    private final int x;
+    private final int y;
+    private final int distance;
+    private final int power;
+
+    public Point(int x, int y, int distance, int power) {
+        this.x = x;
+        this.y = y;
+        this.distance = distance;
+        this.power = power;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public int getPower() {
+        return power;
     }
 }
