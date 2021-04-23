0c900d4f55724010919a5d9887a5b6b637feb6bc
juyeongjung <juyeong.dev@gmail.com>
2021-04-23 18:17:55 +0900

    Correct - union 코드 주의 재할당 되는 코드여야함

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,49 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
 
 public class Main {
 
+    static int[] parent;
+
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int size = Integer.parseInt(reader.readLine());
-        PriorityQueue<Integer> queue = new PriorityQueue<>(Integer::compare);
-        for (int i = 0; i < size; i++) {
-            queue.add(Integer.valueOf(reader.readLine()));
+        String[] init = reader.readLine().split(" ");
+        int size = Integer.parseInt(init[0]);
+        int stateSize = Integer.parseInt(init[1]);
+
+        parent = new int[size + 1];
+        for (int i = 0; i <= size; i++) {
+            parent[i] = i;
         }
 
-        int answer = 0;
-
-        while (!queue.isEmpty()) {
-            int first = queue.remove();
-            if (queue.isEmpty()) {
-                break;
-            }
-            int second = queue.remove();
-            answer += (first + second);
-            if (!queue.isEmpty()) {
-                queue.add(first + second);
+        for (int i = 0; i < stateSize; i++) {
+            String[] inputs = reader.readLine().split(" ");
+            if (inputs[0].equals("0")) {
+                union(Integer.parseInt(inputs[1]), Integer.parseInt(inputs[2]));
+            } else {
+                if (find(Integer.parseInt(inputs[1])) == find(Integer.parseInt(inputs[2]))) {
+                    System.out.println("YES");
+                } else {
+                    System.out.println("NO");
+                }
             }
         }
+    }
 
-        System.out.println(answer);
+    public static int find(int index) {
+        if (parent[index] == index) {
+            return index;
+        } else {
+            return parent[index] = find(parent[index]);
+        }
+    }
+
+    public static void union(int first, int second) {
+        first = find(first);
+        second = find(second);
+        if (first != second) {
+            parent[second] = first;
+        }
     }
 }
