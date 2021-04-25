160ea0758af0fdcc6449e5eb58052c1ed75fe390
juyeongjung <juyeong.dev@gmail.com>
2021-04-25 23:04:10 +0900

    Correct - 입력 EOF만들어야하는 극혐

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,55 +1,70 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int size = Integer.parseInt(reader.readLine());
-
-        int[] parents = new int[size + 1];
-        boolean[] visited = new boolean[size + 1];
-
-        Map<Integer, List<Integer>> tree = new HashMap<>();
-
-        for (int i = 1; i < size; i++) {
-            String[] inputs = reader.readLine().split(" ");
-            int firstInput = Integer.parseInt(inputs[0]);
-            int secondInput = Integer.parseInt(inputs[1]);
-
-            if (!tree.containsKey(firstInput)) {
-                tree.put(firstInput, new ArrayList<>());
+        Node head = null;
+        String input;
+        while ((input = reader.readLine()) != null) {
+            Node node = new Node(Integer.parseInt(input));
+            if (head == null) {
+                head = node;
+            } else {
+                head.add(node);
             }
-            tree.get(firstInput).add(secondInput);
+        }
+        dfs(head);
+    }
 
-            if (!tree.containsKey(secondInput)) {
-                tree.put(secondInput, new ArrayList<>());
+    public static void dfs(Node node) {
+        if (node == null) {
+            return;
+        }
+        dfs(node.getLeft());
+        dfs(node.getRight());
+        System.out.println(node.getData());
+    }
+}
+
+class Node {
+
+    private final int data;
+    private Node left;
+    private Node right;
+
+    public Node(int data) {
+        this.data = data;
+    }
+
+    public void add(Node node) {
+        if (this.data > node.getData()) {
+            if (this.left == null) {
+                this.left = node;
+            } else {
+                this.left.add(node);
             }
-            tree.get(secondInput).add(firstInput);
         }
-        int start = 1;
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
-        while (!queue.isEmpty()) {
-            int nowParent = queue.remove();
-            tree.get(nowParent).forEach(child -> {
-                if (!visited[child]) {
-                    parents[child] = nowParent;
-                    visited[child] = true;
-                    queue.add(child);
-                }
-            });
+        if (this.data < node.getData()) {
+            if (this.right == null) {
+                this.right = node;
+            } else {
+                this.right.add(node);
+            }
         }
+    }
 
-        for (int i = 2; i < parents.length; i++) {
-            System.out.println(parents[i]);
-        }
+    public int getData() {
+        return data;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public Node getRight() {
+        return right;
     }
 }
