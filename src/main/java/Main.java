703cd598175e24c430fa7fd82cd61a7158ca8013
juyeongjung <juyeong.dev@gmail.com>
2021-04-25 23:23:06 +0900

    Correct

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,62 +1,77 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        Node head = null;
-        String input;
-        while ((input = reader.readLine()) != null) {
-            Node node = new Node(Integer.parseInt(input));
-            if (head == null) {
-                head = node;
-            } else {
-                head.add(node);
+        int size = Integer.parseInt(reader.readLine());
+
+        Map<String, Node> tree = new HashMap<>();
+        for (int i = 0; i < size; i++) {
+            String[] inputs = reader.readLine().split(" ");
+            for (String input : inputs) {
+                if (!input.equals(".")) {
+                    tree.putIfAbsent(input, new Node(input));
+                }
+            }
+            if (!inputs[1].equals(".")) {
+                tree.get(inputs[0]).setLeft(tree.get(inputs[1]));
+            }
+            if (!inputs[2].equals(".")) {
+                tree.get(inputs[0]).setRight(tree.get(inputs[2]));
             }
         }
-        dfs(head);
+
+        first(tree.get("A"));
+        System.out.println();
+        mid(tree.get("A"));
+        System.out.println();
+        last(tree.get("A"));
     }
 
-    public static void dfs(Node node) {
+    public static void first(Node node) {
         if (node == null) {
             return;
         }
-        dfs(node.getLeft());
-        dfs(node.getRight());
-        System.out.println(node.getData());
+        System.out.print(node.getData());
+        first(node.getLeft());
+        first(node.getRight());
+    }
+
+    public static void mid(Node node) {
+        if (node == null) {
+            return;
+        }
+        mid(node.getLeft());
+        System.out.print(node.getData());
+        mid(node.getRight());
+    }
+
+    public static void last(Node node) {
+        if (node == null) {
+            return;
+        }
+        last(node.getLeft());
+        last(node.getRight());
+        System.out.print(node.getData());
     }
 }
 
 class Node {
 
-    private final int data;
+    private final String data;
     private Node left;
     private Node right;
 
-    public Node(int data) {
+    public Node(String data) {
         this.data = data;
     }
 
-    public void add(Node node) {
-        if (this.data > node.getData()) {
-            if (this.left == null) {
-                this.left = node;
-            } else {
-                this.left.add(node);
-            }
-        }
-        if (this.data < node.getData()) {
-            if (this.right == null) {
-                this.right = node;
-            } else {
-                this.right.add(node);
-            }
-        }
-    }
-
-    public int getData() {
+    public String getData() {
         return data;
     }
 
@@ -66,5 +81,13 @@ class Node {
 
     public Node getRight() {
         return right;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
     }
 }
