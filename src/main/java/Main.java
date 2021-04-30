8117a84b9568de9ecab8c608ef9c8bfd585bf69d
juyeongjung <juyeong.dev@gmail.com>
2021-04-30 16:08:16 +0900

    Correct - 카메라 그리디

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,54 +1,25 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(solution2(new int[]{30,20,80,70}, 100));
+        System.out.println(solution(new int[][]{{-20, 15}, {-14, -5}, {-18, -13}, {-5, -3}}));
     }
 
-    public static int solution2(int[] people, int limit) {
+    public static int solution(int[][] routes) {
         int answer = 0;
-        int now = people.length - 1;
-        int next = 0;
-        Arrays.sort(people);
-        while (now >= next) {
-            if (people[now] + people[next] <= limit) {
-                next++;
-            }
-            now--;
-            answer++;
-        }
-        return answer;
-    }
-
-    public static int solution(int[] people, int limit) {
-        int answer = 0;
-        PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.comparingInt(x -> (int) x).reversed());
-        for (int weight : people) {
-            queue.add(weight);
-        }
-
-        while (!queue.isEmpty()) {
-            int now = queue.remove();
-            if (now == limit || queue.isEmpty()) {
-                answer++;
-                continue;
-            }
-            List<Integer> keep = new ArrayList<>();
-            while (!queue.isEmpty()) {
-                int next = queue.remove();
-                if (now + next <= limit) {
-                    break;
-                } else {
-                    keep.add(next);
+        Arrays.sort(routes, Comparator.comparingInt(x -> x[0]));
+        int cover = Integer.MIN_VALUE;
+        for (int i = 0; i < routes.length; i++) {
+            int start = routes[i][0];
+            int end = routes[i][1];
+            if (start > cover || end < cover) {
+                if(start > cover) {
+                    answer++;
                 }
+                cover = end;
             }
-            answer++;
-            queue.addAll(keep);
         }
         return answer;
     }
