9146dfbdaecd33f2106e35359537990825716d0e
juyeongjung <juyeong.dev@gmail.com>
2021-04-30 17:42:09 +0900

    fail - 섬연결 런타임

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,85 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(solution(new int[][]{{-20, 15}, {-14, -5}, {-18, -13}, {-5, -3}}));
+        System.out.println(solution(4, new int[][]{{0, 1, 1}, {0, 2, 2}, {0, 3, 3}, {0, 4, 4}, {1, 3, 1}}));
     }
 
-    public static int solution(int[][] routes) {
+    public static int solution(int size, int[][] costs) {
         int answer = 0;
-        Arrays.sort(routes, Comparator.comparingInt(x -> x[0]));
-        int cover = Integer.MIN_VALUE;
-        for (int i = 0; i < routes.length; i++) {
-            int start = routes[i][0];
-            int end = routes[i][1];
-            if (start > cover || end < cover) {
-                if(start > cover) {
-                    answer++;
-                }
-                cover = end;
+        boolean[] visited = new boolean[size + 1];
+        int[] distance = new int[size + 1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        Map<Integer, List<Line>> map = new HashMap<>();
+
+        int start = 0;
+        int best = Integer.MAX_VALUE;
+        for (int i = 0; i < costs.length; i++) {
+            int[] input = costs[i];
+            if (!map.containsKey(input[0])) {
+                map.put(input[0], new ArrayList<>());
+            }
+            map.get(input[0]).add(new Line(input[1], input[2]));
+            if (!map.containsKey(input[1])) {
+                map.put(input[1], new ArrayList<>());
+            }
+            map.get(input[1]).add(new Line(input[0], input[2]));
+
+            if (best > input[2]) {
+                best = input[2];
+                start = i;
             }
         }
+
+        PriorityQueue<Line> queue = new PriorityQueue<>(Comparator.comparingInt(Line::getWeight));
+        queue.add(new Line(start, best));
+        while (!queue.isEmpty()) {
+            Line now = queue.remove();
+            if (visited[now.getEnd()]) {
+                continue;
+            }
+            visited[now.getEnd()] = true;
+            for (Line next : map.get(now.getEnd())) {
+                if (!visited[next.getEnd()] && distance[next.getEnd()] > next.getWeight()) {
+                    distance[next.getEnd()] = next.getWeight();
+                    queue.add(next);
+                }
+            }
+        }
+        distance[start] = 0;
+
+        for (int i = 0; i < distance.length; i++) {
+            if (visited[i]) {
+                answer += distance[i];
+            }
+        }
+
         return answer;
+    }
+}
+
+class Line {
+
+    private final int end;
+    private final int weight;
+
+    public Line(int end, int weight) {
+        this.weight = weight;
+        this.end = end;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getEnd() {
+        return end;
     }
 }
