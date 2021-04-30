1211584e25cca6a2ec45667d02328f364ed50aec
juyeongjung <juyeong.dev@gmail.com>
2021-04-30 14:53:23 +0900

    Correct - greedy인데 PQ쓰면안댐

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,55 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(solution("87654321", 3));
+        System.out.println(solution2(new int[]{30,20,80,70}, 100));
     }
 
-    public static String solution(String number, int deleteCount) {
-        if (number.length() == 1) {
-            return number;
+    public static int solution2(int[] people, int limit) {
+        int answer = 0;
+        int now = people.length - 1;
+        int next = 0;
+        Arrays.sort(people);
+        while (now >= next) {
+            if (people[now] + people[next] <= limit) {
+                next++;
+            }
+            now--;
+            answer++;
         }
-        StringBuilder answer = new StringBuilder();
-        int safeCount = number.length() - deleteCount;
-        int[] numbers = Arrays.stream(number.split("")).mapToInt(Integer::parseInt).toArray();
-        int maxIndex = 0;
-        while (safeCount != 0) {
-            int max = 0;
-            for (int i = maxIndex; i < numbers.length - safeCount + 1; i++) {
-                if (numbers[i] > max) {
-                    max = numbers[i];
-                    maxIndex = i + 1;
+        return answer;
+    }
+
+    public static int solution(int[] people, int limit) {
+        int answer = 0;
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.comparingInt(x -> (int) x).reversed());
+        for (int weight : people) {
+            queue.add(weight);
+        }
+
+        while (!queue.isEmpty()) {
+            int now = queue.remove();
+            if (now == limit || queue.isEmpty()) {
+                answer++;
+                continue;
+            }
+            List<Integer> keep = new ArrayList<>();
+            while (!queue.isEmpty()) {
+                int next = queue.remove();
+                if (now + next <= limit) {
+                    break;
+                } else {
+                    keep.add(next);
                 }
             }
-            answer.append(max);
-            safeCount--;
+            answer++;
+            queue.addAll(keep);
         }
-        return answer.toString();
+        return answer;
     }
 }
