1fcc020e385674d73a2b74373f4cec8013b7552a
juyeongjung <juyeong.dev@gmail.com>
2021-05-03 20:15:58 +0900

    Correct - 인덱스조심

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,35 +8,28 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        int size = Integer.parseInt(reader.readLine());
-        int[] origin = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
-        int inputSize = Integer.parseInt(reader.readLine());
         int[] inputs = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-
-        for (int i = 0; i < inputs.length; i++) {
-            int start = 0;
-            int end = origin.length - 1;
-            int nowIndex = 0;
-            boolean exist = false;
-
-            while (start <= end) {
-                nowIndex = (end + start) / 2;
-                if (inputs[i] == origin[nowIndex]) {
-                    exist = true;
-                    break;
-                } else {
-                    if (inputs[i] < origin[nowIndex]) {
-                        end = nowIndex - 1;
-                    } else {
-                        start = nowIndex + 1;
-                    }
-                }
-            }
-            if (exist) {
-                System.out.println(1);
-            } else {
-                System.out.println(0);
-            }
+        int sinHoCount = inputs[0];
+        int wantCount = inputs[1];
+        int brokenCount = inputs[2];
+        int[] sinHo = new int[sinHoCount];
+        for (int i = 0; i < brokenCount; i++) {
+            sinHo[Integer.parseInt(reader.readLine()) - 1] = 1;
         }
+        int start = 0;
+        int end = wantCount - 1;
+        int sum = 0;
+        for (int i = 0; i <= end; i++) {
+            sum += sinHo[i];
+        }
+        int answer = sum;
+        while (end + 1 < sinHoCount) {
+            int next = sum - sinHo[start] + sinHo[end + 1];
+            answer = Integer.min(next , answer);
+            sum = next;
+            start++;
+            end++;
+        }
+        System.out.println(answer);
     }
 }
