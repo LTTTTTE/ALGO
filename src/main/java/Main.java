e7c4a29c14a981618dbec7270a764e6aa7bd0efb
juyeongjung <juyeong.dev@gmail.com>
2021-05-08 18:55:04 +0900

    Correct - 기본다익스트라

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,136 +1,79 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Stack;
+import java.util.Map;
+import java.util.Optional;
+import java.util.PriorityQueue;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println(solution(8, 2, new String[]{"D 2", "C", "U 3", "C", "D 4", "C", "U 2", "Z", "Z"}));
-    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int pointCount = Integer.parseInt(reader.readLine());
+        int lineCount = Integer.parseInt(reader.readLine());
 
-    public static String solution(int length, int now, String[] cmd) {
-        Point head = new Point(-1);
-        Point nowPoint = null;
-        Point beforePoint = null;
-        for (int i = 0; i < length; i++) {
-            Point point = new Point(i);
-            point.setBefore(beforePoint);
-            if (beforePoint == null) {
-                head.setNext(point);
-                point.setBefore(head);
-            } else {
-                beforePoint.setNext(point);
+        Map<Integer, List<Line>> map = new HashMap<>();
+        for (int i = 0; i < lineCount; i++) {
+            String[] inputs = reader.readLine().split(" ");
+            int start = Integer.parseInt(inputs[0]);
+            if (!map.containsKey(start)) {
+                map.put(start, new ArrayList<>());
             }
-            if (i == now) {
-                nowPoint = point;
-            }
-            beforePoint = point;
+            map.get(start).add(new Line(inputs[1], inputs[2]));
+//
+//            start = Integer.parseInt(inputs[1]);
+//            if (!map.containsKey(start)) {
+//                map.put(start, new ArrayList<>());
+//            }
+//            map.get(start).add(new Line(inputs[0], inputs[2]));
         }
+        String[] inputs = reader.readLine().split(" ");
+        int startPoint = Integer.parseInt(inputs[0]);
+        int endPoint = Integer.parseInt(inputs[1]);
 
-        Stack<Point> deleted = new Stack<>();
-
-        for (int i = 0; i < cmd.length; i++) {
-            if (cmd[i].startsWith("D")) {
-                int value = Integer.parseInt(cmd[i].split(" ")[1]);
-                nowPoint = nowPoint.findNext(value);
-                continue;
-            }
-            if (cmd[i].startsWith("U")) {
-                int value = Integer.parseInt(cmd[i].split(" ")[1]);
-                nowPoint = nowPoint.findBefore(value);
-                continue;
-            }
-            if (cmd[i].equals("C")) {
-                Point before = nowPoint.getBefore();
-                Point next = nowPoint.getNext();
-                if (before != null) {
-                    before.setNext(next);
-                }
-                if (next != null) {
-                    next.setBefore(before);
-                }
-                deleted.push(nowPoint);
-                if (next == null) {
-                    if (before != null) {
-                        nowPoint = before;
-                    }
-                } else {
-                    nowPoint = next;
-                }
-                continue;
-            }
-            if (cmd[i].equals("Z")) {
-                Point pop = deleted.pop();
-                Point before = pop.getBefore();
-                Point next = pop.getNext();
-                if (before != null) {
-                    before.setNext(pop);
-                }
-                if (next != null) {
-                    next.setBefore(pop);
+        int[] distance = new int[pointCount + 1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[startPoint] = 0;
+        PriorityQueue<Line> queue = new PriorityQueue<>(Comparator.comparingInt(Line::getWeight));
+        queue.add(new Line(startPoint, 0));
+        while (!queue.isEmpty()) {
+            Line now = queue.remove();
+            List<Line> lines = Optional.ofNullable(map.get(now.getEnd())).orElse(new ArrayList<>());
+            for (Line line : lines) {
+                if (distance[line.getEnd()] > line.getWeight() + distance[now.getEnd()]) {
+                    distance[line.getEnd()] = line.getWeight() + distance[now.getEnd()];
+                    queue.add(line);
                 }
             }
         }
-        List<String> answer = new ArrayList<>();
-        head = head.getNext();
-        for (int i = 0; i < length; i++) {
-            if (head == null) {
-                answer.add("X");
-                continue;
-            }
-            if (head.getIndex() == i) {
-                answer.add("O");
-            } else {
-                answer.add("X");
-                continue;
-            }
-            head = head.getNext();
-        }
-        return String.join("", answer);
+        System.out.println(distance[endPoint]);
     }
 }
 
-class Point {
+class Line {
 
-    private int index;
-    private Point before;
-    private Point next;
+    private final int end;
+    private final int weight;
 
-    public Point(int index) {
-        this.index = index;
+    public Line(int end, int weight) {
+        this.end = end;
+        this.weight = weight;
     }
 
-    public Point findNext(int count) {
-        if (count == 0) {
-            return this;
-        }
-        return this.next.findNext(count - 1);
+    public Line(String end, String weight) {
+        this(Integer.parseInt(end), Integer.parseInt(weight));
     }
 
-    public Point findBefore(int count) {
-        if (count == 0) {
-            return this;
-        }
-        return this.before.findBefore(count - 1);
+    public int getEnd() {
+        return end;
     }
 
-    public int getIndex() {
-        return index;
-    }
-
-    public Point getBefore() {
-        return before;
-    }
-
-    public void setBefore(Point before) {
-        this.before = before;
-    }
-
-    public Point getNext() {
-        return next;
-    }
-
-    public void setNext(Point next) {
-        this.next = next;
+    public int getWeight() {
+        return weight;
     }
 }
