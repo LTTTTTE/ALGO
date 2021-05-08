44ba275c5efd4f89d7bac84ccef032d61a805b7d
juyeongjung <juyeong.dev@gmail.com>
2021-05-08 14:07:31 +0900

    Correct

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,40 +1,15 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String[] inputs = reader.readLine().split(" ");
-        int studentsCount = Integer.parseInt(inputs[0]);
-        int jewelryCount = Integer.parseInt(inputs[1]);
-        int[] jewelry = new int[jewelryCount];
+    public static void main(String[] args) {
+        System.out.println(solution("2three45sixseven"));
+    }
 
-        for (int i = 0; i < jewelryCount; i++) {
-            jewelry[i] = Integer.parseInt(reader.readLine());
+    public static int solution(String input) {
+        String[] map = new String[]{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        for (int i = 0; i < map.length; i++) {
+            input = input.replaceAll(map[i], String.valueOf(i));
         }
-        int start = 1;
-        int end = Arrays.stream(jewelry).max().getAsInt();
-        int now;
-        int answer = Integer.MAX_VALUE;
-        while (start <= end) {
-            int divideTo = 0;
-            now = (start + end) / 2;
-            for (int i = 0; i < jewelry.length; i++) {
-                divideTo += jewelry[i] / now;
-                if (jewelry[i] % now != 0) {
-                    divideTo++;
-                }
-            }
-            if (divideTo > studentsCount) {
-                start = now + 1;
-            } else {
-                answer = Integer.min(answer, now);
-                end = now - 1;
-            }
-        }
-        System.out.println(answer);
+        return Integer.parseInt(input);
     }
 }
