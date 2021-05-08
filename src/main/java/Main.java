0458aab78b662604e0abb4d093207ed9ee8df9b5
juyeongjung <juyeong.dev@gmail.com>
2021-05-08 16:29:44 +0900

    fail - 효율만다틀림

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,105 +1,190 @@
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
+import java.util.Stack;
 
 public class Main {
 
     public static void main(String[] args) {
-        solution(new String[][]{{"POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"}});
+        System.out.println(solution(8, 2, new String[]{"D 2", "C", "U 3", "C", "D 4", "C", "U 2", "Z", "Z"}));
     }
 
-    public static int[] solution(String[][] places) {
-        List<Integer> answer = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            String[][] network = new String[5][5];
-            for (int j = 0; j < 5; j++) {
-                network[j] = places[i][j].split("");
+    public static String solution(int length, int now, String[] cmd) {
+        Point head = null;
+        Point nowPoint = null;
+        Point beforePoint = null;
+        for (int i = 0; i < length; i++) {
+            Point point = new Point(i);
+            point.setBefore(beforePoint);
+            if (beforePoint == null) {
+                head = point;
             }
-            Point point = findPoint(network, new Point(0, 0));
-            boolean bfs = false;
-            while (point != null) {
-                bfs = bfs(network, point);
-                if (bfs) {
-                    answer.add(0);
-                    break;
-                }
-                point = findPoint(network, point);
+            if (beforePoint != null) {
+                beforePoint.setNext(point);
             }
-            if (!bfs) {
-                answer.add(1);
+            if (i == now) {
+                nowPoint = point;
             }
+            beforePoint = point;
         }
-        return answer.stream().mapToInt(x -> x).toArray();
-    }
 
-    public static boolean bfs(String[][] network, Point startPoint) {
-        int[][] distance = new int[5][5];
-        int[] dx = {1, 0, -1, 0};
-        int[] dy = {0, 1, 0, -1};
-        Queue<Point> queue = new LinkedList<>();
-        queue.add(startPoint);
-        distance[startPoint.getX()][startPoint.getY()] = 0;
-        while (!queue.isEmpty()) {
-            Point now = queue.remove();
-            if (distance[now.getX()][now.getY()] == 2) {
+        Stack<Point> deleted = new Stack<>();
+
+        for (int i = 0; i < cmd.length; i++) {
+            if (cmd[i].startsWith("D")) {
+                int value = Integer.parseInt(cmd[i].split(" ")[1]);
+                nowPoint = nowPoint.findNext(value);
                 continue;
             }
-            for (int i = 0; i < dx.length; i++) {
-                int nextX = now.getX() + dx[i];
-                int nextY = now.getY() + dy[i];
-                if (nextX == startPoint.getX() && nextY == startPoint.getY()) {
-                    continue;
+            if (cmd[i].startsWith("U")) {
+                int value = Integer.parseInt(cmd[i].split(" ")[1]);
+                nowPoint = nowPoint.findBefore(value);
+                continue;
+            }
+            if (cmd[i].equals("C")) {
+                Point before = nowPoint.getBefore();
+                Point next = nowPoint.getNext();
+                if (before != null) {
+                    before.setNext(next);
                 }
-                if (nextX < 0 || nextY < 0 || nextX >= 5 || nextY >= 5) {
-                    continue;
+                if (next != null) {
+                    next.setBefore(before);
                 }
-                if (network[nextX][nextY].equals("X")) {
-                    continue;
+                deleted.push(nowPoint);
+                if (next == null) {
+                    if (before != null) {
+                        nowPoint = before;
+                    }
                 }
-                if (network[nextX][nextY].equals("O") && distance[nextX][nextY] == 0) {
-                    queue.add(new Point(nextX, nextY));
-                    distance[nextX][nextY] = distance[now.getX()][now.getY()] + 1;
+                continue;
+            }
+            if (cmd[i].equals("Z")) {
+                Point pop = deleted.pop();
+                Point before = pop.getBefore();
+                Point next = pop.getNext();
+                if (before != null) {
+                    before.setNext(pop);
                 }
-                if (network[nextX][nextY].equals("P")) {
-                    return true;
+                if (next != null) {
+                    next.setBefore(pop);
                 }
             }
         }
-
-        return false;
-    }
-
-    public static Point findPoint(String[][] network, Point beforePoint) {
-        int nowX = beforePoint.getX();
-        int nextY = beforePoint.getY() + 1;
-        for (int i = nowX; i < 5; i++) {
-            for (int j = nextY; j < 5; j++) {
-                if (network[i][j].equals("P")) {
-                    return new Point(i, j);
-                }
+        List<String> answer = new ArrayList<>();
+        for (int i = 0; i < length; i++) {
+            if (head.getIndex() == i) {
+                answer.add("O");
+            } else {
+                answer.add("X");
+                continue;
             }
-            nextY = 0;
+            head = head.getNext();
         }
-        return null;
+        return String.join("", answer);
     }
+    //    public static String solution2(int length, int now, String[] cmd) {
+//        now = now + 1;
+//        Map<Integer, Boolean> map = new HashMap<>();
+//        for (int i = 1; i <= length; i++) {
+//            map.put(i, true);
+//        }
+//        Stack<Integer> deleted = new Stack<>();
+//        for (int i = 0; i < cmd.length; i++) {
+//            if (cmd[i].startsWith("D")) {
+//                int value = Integer.parseInt(cmd[i].split(" ")[1]);
+//                for (int k = 1; k <= value; k++) {
+//                    if (!map.get(now + k)) {
+//                        value++;
+//                    }
+//                }
+//                now += value;
+//                continue;
+//            }
+//            if (cmd[i].startsWith("U")) {
+//                int value = Integer.parseInt(cmd[i].split(" ")[1]);
+//                for (int k = 1; k <= value; k++) {
+//                    if (!map.get(now - k)) {
+//                        value++;
+//                    }
+//                }
+//                now -= value;
+//                continue;
+//            }
+//            if (cmd[i].equals("C")) {
+//                map.replace(now, false);
+//                deleted.push(now);
+//                int value = 1;
+//                while (value + now <= length) {
+//                    if (!map.get(now + value)) {
+//                        value++;
+//                    } else {
+//                        break;
+//                    }
+//                }
+//                if (now + value <= length) {
+//                    now += value;
+//                } else {
+//                    value = 0;
+//                    while (value < now) {
+//                        if (!map.get(now - value)) {
+//                            value++;
+//                        } else {
+//                            break;
+//                        }
+//                    }
+//                    now -= value;
+//                }
+//                continue;
+//            }
+//            if (cmd[i].equals("Z")) {
+//                int value = deleted.pop();
+//                map.replace(value, true);
+//            }
+//        }
+//        return map.values().stream().map(x -> x ? "O" : "X").collect(Collectors.joining());
+//    }
 }
 
 class Point {
 
-    private final int x;
-    private final int y;
+    private int index;
+    private Point before;
+    private Point next;
 
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
+    public Point(int index) {
+        this.index = index;
     }
 
-    public int getX() {
-        return x;
+    public Point findNext(int count) {
+        if (count == 0) {
+            return this;
+        }
+        return this.next.findNext(count - 1);
     }
 
-    public int getY() {
-        return y;
+    public Point findBefore(int count) {
+        if (count == 0) {
+            return this;
+        }
+        return this.before.findBefore(count - 1);
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public Point getBefore() {
+        return before;
+    }
+
+    public void setBefore(Point before) {
+        this.before = before;
+    }
+
+    public Point getNext() {
+        return next;
+    }
+
+    public void setNext(Point next) {
+        this.next = next;
     }
 }
