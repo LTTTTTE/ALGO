81969706fd2ba0e18e1b5063363ff9a5f2827520
juyeongjung <juyeong.dev@gmail.com>
2021-05-08 21:36:10 +0900

    Correct - 최소 스패닝트리 기본

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,72 +1,76 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 import java.util.PriorityQueue;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int pointCount = Integer.parseInt(reader.readLine());
-        int lineCount = Integer.parseInt(reader.readLine());
+        String[] init = reader.readLine().split(" ");
+        int pointCount = Integer.parseInt(init[0]);
+        int lineCount = Integer.parseInt(init[1]);
+        int[] parents = new int[pointCount + 1];
 
-        Map<Integer, List<Line>> map = new HashMap<>();
+        for (int i = 0; i < parents.length; i++) {
+            parents[i] = i;
+        }
+
+        PriorityQueue<Line> queue = new PriorityQueue<>(Comparator.comparingInt(Line::getWeight));
         for (int i = 0; i < lineCount; i++) {
             String[] inputs = reader.readLine().split(" ");
-            int start = Integer.parseInt(inputs[0]);
-            if (!map.containsKey(start)) {
-                map.put(start, new ArrayList<>());
-            }
-            map.get(start).add(new Line(inputs[1], inputs[2]));
-//
-//            start = Integer.parseInt(inputs[1]);
-//            if (!map.containsKey(start)) {
-//                map.put(start, new ArrayList<>());
-//            }
-//            map.get(start).add(new Line(inputs[0], inputs[2]));
+            queue.add(new Line(inputs[0], inputs[1], inputs[2]));
         }
-        String[] inputs = reader.readLine().split(" ");
-        int startPoint = Integer.parseInt(inputs[0]);
-        int endPoint = Integer.parseInt(inputs[1]);
 
-        int[] distance = new int[pointCount + 1];
-        Arrays.fill(distance, Integer.MAX_VALUE);
-        distance[startPoint] = 0;
-        PriorityQueue<Line> queue = new PriorityQueue<>(Comparator.comparingInt(Line::getWeight));
-        queue.add(new Line(startPoint, 0));
+        int answer = 0;
         while (!queue.isEmpty()) {
             Line now = queue.remove();
-            List<Line> lines = Optional.ofNullable(map.get(now.getEnd())).orElse(new ArrayList<>());
-            for (Line line : lines) {
-                if (distance[line.getEnd()] > line.getWeight() + distance[now.getEnd()]) {
-                    distance[line.getEnd()] = line.getWeight() + distance[now.getEnd()];
-                    queue.add(line);
-                }
+            int startRoot = find(parents, now.getStart());
+            int endRoot = find(parents, now.getEnd());
+            if (startRoot != endRoot) {
+                union(parents, now.getStart(), now.getEnd());
+                answer += now.getWeight();
             }
         }
-        System.out.println(distance[endPoint]);
+        System.out.println(answer);
+    }
+
+    public static int find(int[] parent, int child) {
+        if (parent[child] == child) {
+            return child;
+        }
+        parent[child] = find(parent, parent[child]);
+        return parent[child];
+    }
+
+    public static void union(int[] parent, int first, int second) {
+        int firstRoot = find(parent, first);
+        int secondRoot = find(parent, second);
+        if (firstRoot != secondRoot) {
+            parent[secondRoot] = firstRoot;
+        }
     }
 }
 
 class Line {
 
+    private final int start;
     private final int end;
     private final int weight;
 
-    public Line(int end, int weight) {
+    public Line(int start, int end, int weight) {
+        this.start = start;
         this.end = end;
         this.weight = weight;
     }
 
-    public Line(String end, String weight) {
-        this(Integer.parseInt(end), Integer.parseInt(weight));
+    public Line(String start, String end, String weight) {
+        this(Integer.parseInt(start), Integer.parseInt(end), Integer.parseInt(weight));
+    }
+
+    public int getStart() {
+        return start;
     }
 
     public int getEnd() {
