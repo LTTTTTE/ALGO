a141d3cfcc404b4c65020bc1cb52685d7fd69c4d
juyeongjung <juyeong.dev@gmail.com>
2021-05-15 00:00:05 +0900

    Correct - 기본 플로이드

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,13 +7,15 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int nodeSize = Integer.parseInt(reader.readLine());
-        int lineSize = Integer.parseInt(reader.readLine());
-
+        String[] inits = reader.readLine().split(" ");
+        int nodeSize = Integer.parseInt(inits[0]);
+        int wantLength = Integer.parseInt(inits[1]);
+        int lineSize = Integer.parseInt(inits[2]);
+        int[] items = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int[][] distance = new int[nodeSize][nodeSize];
 
-        for (int i = 0; i < distance.length; i++) {
-            Arrays.fill(distance[i], Integer.MAX_VALUE / 2);
+        for (int[] i : distance) {
+            Arrays.fill(i, Integer.MAX_VALUE / 2);
         }
 
         for (int i = 0; i < distance.length; i++) {
@@ -27,26 +29,27 @@ public class Main {
         for (int i = 0; i < lineSize; i++) {
             int[] inputs = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             distance[inputs[0] - 1][inputs[1] - 1] = Integer.min(distance[inputs[0] - 1][inputs[1] - 1], inputs[2]);
+            distance[inputs[1] - 1][inputs[0] - 1] = Integer.min(distance[inputs[1] - 1][inputs[0] - 1], inputs[2]);
         }
 
         for (int mid = 0; mid < nodeSize; mid++) {
             for (int start = 0; start < nodeSize; start++) {
                 for (int end = 0; end < nodeSize; end++) {
-                    int before = distance[start][mid] + distance[mid][end];
-                    distance[start][end] = Integer.min(before, distance[start][end]);
+                    distance[start][end] = Integer.min(distance[start][mid] + distance[mid][end], distance[start][end]);
                 }
             }
         }
-
-        for (int[] i : distance) {
-            for (int j : i) {
-                if (j == Integer.MAX_VALUE / 2) {
-                    System.out.print(0 + " ");
-                } else {
-                    System.out.print(j + " ");
+        int[] answer = new int[distance.length];
+        for (int i = 0; i < distance.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < distance[i].length; j++) {
+                if (distance[i][j] > wantLength) {
+                    continue;
                 }
+                sum += items[j];
             }
-            System.out.println();
+            answer[i] = sum;
         }
+        System.out.println(Arrays.stream(answer).max().getAsInt());
     }
 }
