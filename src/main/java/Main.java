750efb5daa3b652965ff24cf9792265cdb0aa0c7
juyeongjung <juyeong.dev@gmail.com>
2021-05-17 22:55:52 +0900

    Correct - 위상정렬 응용 - EOF극혐

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,56 +2,70 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Queue;
-import java.util.stream.Collectors;
+import java.util.PriorityQueue;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String[] inits = reader.readLine().split(" ");
-        int nodeSize = Integer.parseInt(inits[0]);
-        int lineSize = Integer.parseInt(inits[1]);
-        int[] beforeSize = new int[nodeSize];
-
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        for (int i = 0; i < lineSize; i++) {
-            int[] inputs = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).map(x -> x - 1)
-                .toArray();
-            if (!map.containsKey(inputs[0])) {
-                map.put(inputs[0], new ArrayList<>());
+        String input = reader.readLine();
+        Map<String, List<String>> map = new HashMap<>();
+        Map<String, Integer> time = new HashMap<>();
+        Map<String, Integer> beforeSize = new HashMap<>();
+        Map<String, Integer> total = new HashMap<>();
+        Set<String> noLeaf = new HashSet<>();
+        do {
+            String[] inputs = input.split(" ");
+            time.put(inputs[0], Integer.parseInt(inputs[1]));
+            if (inputs.length > 2) {
+                String[] beforeInputs = inputs[2].split("");
+                beforeSize.put(inputs[0], beforeInputs.length);
+                for (String before : beforeInputs) {
+                    noLeaf.add(before);
+                    if (!map.containsKey(before)) {
+                        map.put(before, new ArrayList<>());
+                    }
+                    map.get(before).add(inputs[0]);
+                }
+            } else {
+                beforeSize.put(inputs[0], 0);
             }
-            map.get(inputs[0]).add(inputs[1]);
-            beforeSize[inputs[1]]++;
-        }
+        } while ((input = reader.readLine()) != null);
+        Set<String> leaf = new HashSet<>(time.keySet());
+        leaf.removeAll(noLeaf);
 
-        Queue<Integer> queue = new LinkedList<>();
-        List<Integer> answer = new ArrayList<>();
-        for (int i = 0; i < beforeSize.length; i++) {
-            if (beforeSize[i] == 0) {
-                queue.add(i);
-                answer.add(i);
+        PriorityQueue<String> queue = new PriorityQueue<>(Comparator.comparingInt(time::get));
+        for (String each : beforeSize.keySet()) {
+            if (beforeSize.get(each).equals(0)) {
+                queue.add(each);
             }
+            total.put(each, time.get(each));
         }
 
         while (!queue.isEmpty()) {
-            int now = queue.remove();
-            List<Integer> nextNodes = Optional.ofNullable(map.get(now)).orElse(new ArrayList<>());
+            String now = queue.remove();
+            List<String> nextNodes = Optional.ofNullable(map.get(now)).orElse(new ArrayList<>());
+            for (String next : nextNodes) {
+                int cost = Integer.max(total.get(next), total.get(now) + time.get(next));
+                total.put(next, cost);
+                beforeSize.put(next, beforeSize.get(next) - 1);
 
-            for (int next : nextNodes) {
-                beforeSize[next]--;
-                if (beforeSize[next] == 0) {
+                if (beforeSize.get(next).equals(0)) {
                     queue.add(next);
-                    answer.add(next);
                 }
             }
         }
-        System.out.println(answer.stream().map(x -> x + 1).map(String::valueOf).collect(Collectors.joining(" ")));
+        int answer = Integer.MIN_VALUE;
+        for (String each : leaf) {
+            answer = Integer.max(total.get(each), answer);
+        }
+        System.out.println(answer);
     }
 }
