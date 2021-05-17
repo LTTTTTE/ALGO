1c9636a5db0eba3b7f56f20ac06c1fc1e45000c0
juyeongjung <juyeong.dev@gmail.com>
2021-05-17 21:28:15 +0900

    Correct - 위상정렬 기본

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,52 +9,49 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Queue;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int caseSize = Integer.parseInt(reader.readLine());
-        for (int c = 0; c < caseSize; c++) {
-            int[] inits = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            int nodeSize = inits[0];
-            int lineSize = inits[1];
-            int[] needTimes = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            int[] eachTimes = new int[nodeSize];
-            int[] beforeSize = new int[nodeSize];
-            Map<Integer, List<Integer>> map = new HashMap<>();
-            for (int i = 0; i < lineSize; i++) {
-                int[] inputs = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).map(x -> x - 1)
-                    .toArray();
-                if (!map.containsKey(inputs[0])) {
-                    map.put(inputs[0], new ArrayList<>());
-                }
-                map.get(inputs[0]).add(inputs[1]);
-                beforeSize[inputs[1]]++;
-            }
-            int endPoint = Integer.parseInt(reader.readLine()) - 1;
+        String[] inits = reader.readLine().split(" ");
+        int nodeSize = Integer.parseInt(inits[0]);
+        int lineSize = Integer.parseInt(inits[1]);
+        int[] beforeSize = new int[nodeSize];
 
-            Queue<Integer> queue = new LinkedList<>();
-            for (int i = 0; i < beforeSize.length; i++) {
-                if (beforeSize[i] == 0) {
-                    queue.add(i);
-                }
-                eachTimes[i] = needTimes[i];
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < lineSize; i++) {
+            int[] inputs = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).map(x -> x - 1)
+                .toArray();
+            if (!map.containsKey(inputs[0])) {
+                map.put(inputs[0], new ArrayList<>());
             }
-            while (!queue.isEmpty()) {
-                int now = queue.remove();
-                List<Integer> nextNodes = Optional.ofNullable(map.get(now)).orElse(new ArrayList<>());
-
-                for (int next : nextNodes) {
-                    eachTimes[next] = Integer.max(needTimes[next] + eachTimes[now], eachTimes[next]);
-                    beforeSize[next]--;
-
-                    if (beforeSize[next] == 0) {
-                        queue.add(next);
-                    }
-                }
-            }
-            System.out.println(eachTimes[endPoint]);
+            map.get(inputs[0]).add(inputs[1]);
+            beforeSize[inputs[1]]++;
         }
+
+        Queue<Integer> queue = new LinkedList<>();
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < beforeSize.length; i++) {
+            if (beforeSize[i] == 0) {
+                queue.add(i);
+                answer.add(i);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int now = queue.remove();
+            List<Integer> nextNodes = Optional.ofNullable(map.get(now)).orElse(new ArrayList<>());
+
+            for (int next : nextNodes) {
+                beforeSize[next]--;
+                if (beforeSize[next] == 0) {
+                    queue.add(next);
+                    answer.add(next);
+                }
+            }
+        }
+        System.out.println(answer.stream().map(x -> x + 1).map(String::valueOf).collect(Collectors.joining(" ")));
     }
 }
